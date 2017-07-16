4c267f78c7a5fd7b563ba1468fe62a78887134f0
Giovanni Petrache <giovanni.petrache@codingavenue.com>
2017-07-16 09:47:38 +0800

    Api

--- src/AppBundle/Controller/BusinessController.php
+++ src/AppBundle/Controller/BusinessController.php
@@ -2,6 +2,8 @@
 
 namespace AppBundle\Controller;
 
+use UnionBank\Api\Financial;
+
 use AppBundle\Entity\Business;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
@@ -17,9 +19,16 @@ class BusinessController extends Controller
      */
     public function informationAction(Request $request)
     {
+        $financial_api = new Financial();
+
+        $account1 = $financial_api->createTestAccount("Juan dela Cruz");
+        $account2 = $financial_api->createTestAccount("Maria Santos");
+
         // replace this example code with whatever you need
         return $this->render('AppBundle::Business/information.html.twig', [
-            'page_title' => 'Information'
+            'page_title' => 'Information',
+            'account1' => $account1,
+            'account2' => $account2,
         ]);
     }
 }
